6543beddac3f87e3e41961c2b8ddcca8742ffeca
Yauheniya Nikulyak <ynikulyak@csumb.edu>
2020-04-09 19:00:52 -0700

    Removing a space

--- src/main/java/cst438/Team11ProjectFlightGeneralInfoServiceApplication.java
+++ src/main/java/cst438/Team11ProjectFlightGeneralInfoServiceApplication.java
@@ -9,5 +9,4 @@ public class Team11ProjectFlightGeneralInfoServiceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(Team11ProjectFlightGeneralInfoServiceApplication.class, args);
 	}
-
 }
